83a4cc6ebc048d9078147b2719f2edfbaf5e5efc
Adam Chyciński <24207194+Seseikelele@users.noreply.github.com>
2020-07-20 15:14:35 +0200

    Don't be silly, wrap your Willy

--- crypto/kcv.sh
+++ crypto/kcv.sh
@@ -1,11 +1,30 @@
-if [ -e key ]
-then
-	echo using key `cat key`
-else
-	perl -E "print '0' x 48" > key
-	echo 'created null 3des key'
-fi
-perl -E "print \"\x00\" x 64" > .nulls
-KCV=$(openssl enc -K `cat key` -in .nulls -des-ede3 | xxd -g 3 -l 3 -u -p)
-echo "KCV: $KCV"
+#!/usr/bin/env bash
+#https://ricardoanderegg.com/posts/bash_wrap_functions/
+#https://linuxhint.com/wait_command_linux/
+
+function check_key_exists()
+{
+	if [ -e key ]
+	then
+		echo using key `cat key`
+	else
+		perl -E "print '0' x 48" > key
+		echo 'created null 3des key'
+	fi
+}
+
+function generate_nulls()
+{
+	perl -E "print \"\x00\" x 64" > .nulls
+}
+
+function calculate_kcv()
+{
+	KCV=$(openssl enc -K `cat key` -in .nulls -des-ede3 | xxd -g 3 -l 3 -u -p)
+	echo "KCV: $KCV"
+}
+
+check_key_exists & generate_nulls
+wait
+calculate_kcv
 rm -f .nulls
