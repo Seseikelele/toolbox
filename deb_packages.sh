749d2584b3d5dada5d9d05dda0da7902b5d0715b
Adam Chyciński <24207194+Seseikelele@users.noreply.github.com>
2020-07-21 09:33:14 +0200

    packages to install

---
+++ deb_packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# INSTALL PACKAGES
+sudo apt-get install  \
+vim                   \
+firmware-iwlwifi      \
+git                   \
+keepassxc
+
+# REMOVE PACKAGES
+sudo apt-get remove   \
+xterm
