b370c77f1ee671ae95cfb005b4040f333863e57b
Adam Chyciński <24207194+Seseikelele@users.noreply.github.com>
2021-04-17 00:25:05 +0200

    Create vscode_extensions.sh

---
+++ vscode_extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+code --install-extension ms-python.python
+
+code --install-extension aaron-bond.better-comments
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension emeraldwalk.runonsave
