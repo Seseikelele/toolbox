7e4d2f79091752d3c6d1e3c767da8d8517e4bdfa
Seseikelele <email = 24207194+Seseikelele@users.noreply.github.com>
2021-05-20 13:10:08 +0200

    logging, silent curl

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,33 +7,39 @@ create_link() {
 	ln -sf $BASEDIR/$1 $2
 }
 
-install_vim_files() {
-	echo "Checking for neovim"
-	[ -e /bin/nvim ] && {
-		echo "Installing for neovim";
-		create_link init.vim ~/.config/nvim/init.vim;
-		curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-		echo "Done for neovim";
-	}
-
-	echo "Checking for !neovim and vim"
-	[ ! -e /bin/nvim ] && [ -e /bin/vim ] && {
-		echo "Installing for vim";
-		create_link init.vim ~/.vimrc;
-		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-		echo "Done for vim";
-	}
+log() {
+	echo -e $@
 }
 
-echo "BASH"
+install_vim_files() {
+	log '\tChecking for neovim'
+	[ -e /bin/nvim ] && {
+		log '\tInstalling for neovim';
+		create_link init.vim ~/.config/nvim/init.vim;
+		curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+		log '\tDone for neovim';
+	}
+
+	log '\tChecking for !neovim and vim'
+	[ ! -e /bin/nvim ] && [ -e /bin/vim ] && {
+		log '\tInstalling for vim';
+		create_link init.vim ~/.vimrc;
+		curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+		log '\tDone for vim';
+	}
+
+	log '\tdone'
+}
+
+log 'BASH'
 create_link bash/aliases ~/.bash_aliases
-echo "BASH DONE"
+log 'BASH DONE'
 
-echo "GIT"
+log 'GIT'
 create_link gitconfig ~/.gitconfig
-echo "GIT DONE"
+log 'GIT DONE'
 
-echo "VIM"
+log 'VIM'
 install_vim_files
 # :PlugInstall
-echo "VIM DONE"
+log 'VIM DONE'
