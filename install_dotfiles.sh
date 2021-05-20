5c9b5f4b44fc05b93afcd332393cbaac825139fb
Seseikelele <email = 24207194+Seseikelele@users.noreply.github.com>
2021-05-20 10:33:36 +0000

    (neo)vim plugins

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,5 +3,28 @@ set -euo pipefail
 
 BASEDIR=$(realpath ./dotfiles)
 
+install_vim_files() {
+	echo "Checking for neovim"
+	[ -e /bin/nvim ] && {
+		echo "Installing for neovim";
+		ln -sf $BASEDIR/init.vim ~/.config/nvim/init.vim;
+		curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+		echo "Done for neovim";
+	}
+
+	echo "Checking for !neovim and vim"
+	[ ! -e /bin/nvim ] && [ -e /bin/vim ] && {
+		echo "Installing for vim";
+		ln -sf $BASEDIR/init.vim ~/.vimrc;
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+		echo "Done for vim";
+	}
+}
+
+echo "GIT"
 ln -sf $BASEDIR/.gitconfig ~/.gitconfig
-ln -sf $BASEDIR/init.vim ~/.config/nvim/init.vim
+echo "GIT DONE"
+echo "VIM"
+install_vim_files
+# :PlugInstall
+echo "VIM DONE"
