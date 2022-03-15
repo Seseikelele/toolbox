93322276c58ae629f50a796056b4bda928f25322
Seseikelele <24207194+Seseikelele@users.noreply.github.com>
2022-03-16 00:34:43 +0100

    dotfiles tiny cleanup

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -11,35 +11,15 @@ log() {
 	echo -e $@
 }
 
-install_vim_files() {
-	log '\tChecking for neovim'
-	[ -e /bin/nvim ] && {
-		log '\tInstalling for neovim';
-		create_link init.vim ~/.config/nvim/init.vim;
-		curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-		log '\tDone for neovim';
-	}
-
-	log '\tChecking for !neovim and vim'
-	[ ! -e /bin/nvim ] && [ -e /bin/vim ] && {
-		log '\tInstalling for vim';
-		create_link init.vim ~/.vimrc;
-		curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-		log '\tDone for vim';
-	}
-
-	log '\tdone'
-}
+log 'VIM'
+create_link vimrc ~/.vimrc
+log 'VIM DONE'
 
 log 'BASH'
-create_link bash/aliases ~/.bash_aliases
+create_link bash_aliases ~/.bash_aliases
 log 'BASH DONE'
 
 log 'GIT'
 create_link gitconfig ~/.gitconfig
 log 'GIT DONE'
 
-log 'VIM'
-install_vim_files
-# :PlugInstall
-log 'VIM DONE'
